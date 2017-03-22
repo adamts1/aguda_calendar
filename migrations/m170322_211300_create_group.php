91fe3765d7867f98e94075e10b8d561c9a1b62a2
laury <laury.aziza@gmail.com>
2017-03-22 21:16:22 +0200

    fix migrations group table

---
+++ migrations/m170322_211300_create_group.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m170322_211300_create_group extends Migration
+{
+   public function up()
+    {
+        $this->addColumn('group', 'day', $this->string());
+        $this->addColumn('group', 'start', $this->time());
+        $this->addColumn('group', 'end', $this->time());
+
+         $this->dropColumn('group', 'dayintheweek');
+    }
+
+    public function down()
+    {
+        $this->dropColumn('group', 'day');
+        $this->dropColumn('group', 'start');
+        $this->dropColumn('group', 'end');
+
+        $this->addColumn('group', 'dayintheweek', $this->string());
+    }
+
+}
