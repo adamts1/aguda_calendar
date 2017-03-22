a2b112bf9dee9b30b70b6453b3876aa40c57389d
laury <laury.aziza@gmail.com>
2017-03-22 18:43:22 +0200

    laury8

--- views/group/_form.php
+++ views/group/_form.php
@@ -7,6 +7,7 @@ use app\models\Location;
 use app\models\User;
 use app\models\Student;
 use yii\jui\DatePicker;
+
 //check
 /* @var $this yii\web\View */
 /* @var $model app\models\Group */
@@ -23,15 +24,55 @@ use yii\jui\DatePicker;
 
    <?= $form->field($model, 'courseid')->dropDownList(Course::getCourse()) ?>
 
-    <?= $form->field($model, 'teacherid')->dropDownList(User::getTeachers()) ?>
+   <?= $form->field($model, 'teacherid')->dropDownList(User::getTeachers()) ?>
 
-    <?= $form->field($model, 'locationid')->dropDownList(Location::getLocation()) ?>
+   <?= $form->field($model, 'locationid')->dropDownList(Location::getLocation()) ?>
+    
+   <?=  $form->field($model, 'dayintheweek')->dropDownList([
+            '1'=>'Monday',
+            '2'=>'Tuesday', 
+            '3'=>'Wednesday',
+            '4'=>'Thursday',
+            '5'=>'Friday',
+            '6'=>'Saturday',
+            '7'=>'Sunday'
+            ]) ?>
+
+   <?=  $form->field($model, 'duration')->dropDownList([
+            '1'=>'8:00',
+            '2'=>'8:30',
+            '3'=>'9:00',
+            '4'=>'9:30',
+            '5'=>'10:00',
+            '6'=>'10:30',
+            '7'=>'11:00',
+            '8'=>'11:30',
+            '9'=>'12:00',
+            '10'=>'12:30',
+            '11'=>'13:00',
+            '12'=>'13:30',
+            '13'=>'14:00',
+            '14'=>'14:30',
+            '15'=>'15:00',
+            '16'=>'15:30',
+            '17'=>'16:00',
+            '18'=>'16:30',
+            '19'=>'17:00',
+            '20'=>'17:30',
+            '21'=>'18:00',
+
+            ]) ?>
+
+    <?=  $form->field($model, 'duration')->dropDownList([
+            '1'=>'1:00',
+            '2'=>'1:30',
+            '3'=>'2:00',
+            '4'=>'2:30',
+            '5'=>'3:00',
+            ]) ?>
+ 
 
  <!-- <?//= $form->field($model, 'locationid')->dropDownList(Student::getStudentForGroup()) ?> -->
-    
-    <?= $form->field($model, 'dayintheweek')->textInput(['maxlength' => true]) ?>
-
-   
 
 
     <div class="form-group ">
