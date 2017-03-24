851663b0209fc6081e489525e264d3f327793e36
adamts <adamtsityat@gmail.com>
2017-03-24 11:09:14 +0300

    merge

--- views/group/_form.php
+++ views/group/_form.php
@@ -11,10 +11,6 @@ use yii\jui\DatePicker;
 use \kartik\time\TimePicker;
 use kartik\select2\Select2;
 
-
-
-
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Group */
 /* @var $form yii\widgets\ActiveForm */
@@ -28,8 +24,17 @@ use kartik\select2\Select2;
 
    <?= $form->field($model, 'teacherid')->dropDownList(User::getTeachers()) ?>
 
-
    <?= $form->field($model, 'locationid')->dropDownList(Location::getLocation()) ?>
+    
+   <?=  $form->field($model, 'day')->dropDownList([
+            '1'=>'Monday',
+            '2'=>'Tuesday', 
+            '3'=>'Wednesday',
+            '4'=>'Thursday',
+            '5'=>'Friday',
+            '6'=>'Saturday',
+            '7'=>'Sunday'
+            ]) ?>
 
    <?=  $form->field($model, 'duration')->dropDownList([
             '1'=>'8:00',
@@ -55,11 +60,19 @@ use kartik\select2\Select2;
             '21'=>'18:00',
 
             ]) ?>
-    
-  
+
+    <?=  $form->field($model, 'duration')->dropDownList([
+            '1'=>'1:00',
+            '2'=>'1:30',
+            '3'=>'2:00',
+            '4'=>'2:30',
+            '5'=>'3:00',
+            ]) ?>
+ 
 
  <!-- <?//= $form->field($model, 'locationid')->dropDownList(Student::getStudentForGroup()) ?> -->
 
+   <?= $form->field($model, 'day')->dropDownList(['א' => 'א', 'ב' => 'ב', 'ג' => 'ג', 'ד' => 'ד', 'ה' => 'ה', 'ו' => 'ו', 'ש' => 'ש']); ?>
 
    <?=  $form->field($model, 'start')->widget(TimePicker::classname(), [
     'pluginOptions' => [
